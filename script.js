aa8559a7e66a9fab7f84f9283c7a842c6367d4f4
RihabMhd <rihabmahdi19@gmail.com>
2025-11-21 09:26:27 +0100

    feat(script): add functions displayEmployee, hideTable, showTable to manipulate the dataset

---
+++ script.js
@@ -0,0 +1,30 @@
+const parentDiv=document.getElementById('employees-container');
+
+function displayEmployee(employees){
+    const list=document.getElementById('employees-tbody');
+    employees.forEach((emp) => {
+        const tr=document.createElement('tr');
+        tr.innerHTML=`<td>${emp.firstName}</td><td>${emp.lastName}</td>
+        <td>${emp.department}</td><td>${emp.position}</td><td>${emp.salary}</td>
+        <td>${emp.skills.join("-")}</td><td>${emp.joinDate}</td><td>${emp.email}</td>
+        <td>${emp.projects.join('-')}</td><td>${emp.isActive}</td><td>${emp.languages.join('-')}</td>`
+        list.appendChild(tr)
+    });
+}
+displayEmployee(employees)
+
+function hideTable(){
+    const hide = document.getElementById('hide');
+    hide.addEventListener('click',()=>{
+        parentDiv.style.display='none';
+    })
+}
+
+function showTable(){
+    const show = document.getElementById('show');
+    show.addEventListener('click',()=>{
+        parentDiv.style.display='block';
+    })
+}
+hideTable()
+showTable()
