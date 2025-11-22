873e84541be0e7deae6347de24eb5e321cd51f1b
RihabMhd <rihabmahdi19@gmail.com>
2025-11-22 14:19:05 +0100

    Arrays Manipulation

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const employeeForm = document.getElementById("employee-form");
 
 function displayEmployee(employees) {
     const list = document.getElementById('employees-tbody');
+    list.innerHTML = "";
     employees.forEach((emp) => {
         const tr = document.createElement('tr');
         tr.innerHTML = `<td>${emp.firstName}</td><td>${emp.lastName}</td>
@@ -66,16 +67,17 @@ hideTable()
 //     displayEmployee(employees);
 
 // }
-function getSkills(){
-    let skillsArray=[];
-    const skills=document.querySelectorAll('.skill')
-    skills.forEach((skill)=>{
+function getSkills() {
+    let skillsArray = [];
+    const skills = document.querySelectorAll('.skill')
+    skills.forEach((skill) => {
         skillsArray.push(skill.value)
     })
     return skillsArray
 }
+
 function formHandling() {
-    
+
     buttonDynamique.addEventListener('click', () => {
         const div = document.createElement('div');
         div.innerHTML = `
@@ -94,6 +96,8 @@ function formHandling() {
             department: document.getElementById('department').value.trim(),
             skills: getSkills()
         };
+        if (!validation()) return;
+
 
         employees.push(newEmployee);
 
@@ -101,3 +105,90 @@ function formHandling() {
     })
 }
 formHandling()
+
+function validation() {
+    let isValid = true;
+    const firstName = document.getElementById('firstName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+    const age = document.getElementById('age').value.trim();
+    const firstNameError = document.getElementById('firstName-error');
+    const lastNameError = document.getElementById('lastName-error');
+    const ageError = document.getElementById('age-error');
+    const nameRegex = /^[a-zA-Z]{3,}$/;
+    const ageRegex = /^[2-5]|(60)$/
+    if (!nameRegex.test(firstName)) {
+        firstNameError.classList.remove('hidden');
+        firstNameError.textContent = 'Enter a Valid Name';
+        isValid = false;
+    } else {
+        firstNameError.classList.add('hidden');
+        firstNameError.textContent = '';
+        isValid = true;
+    }
+
+    if (!nameRegex.test(lastName)) {
+        lastNameError.classList.remove('hidden');
+        lastNameError.textContent = 'Enter a Valid Name';
+        isValid = false;
+    } else {
+        lastNameError.classList.add('hidden');
+        lastNameError.textContent = '';
+        isValid = true;
+    }
+
+    if (!ageRegex.test(age)) {
+        ageError.classList.remove('hidden');
+        ageError.textContent = 'Enter a Valid Age';
+        isValid = false;
+    } else {
+        ageError.classList.add('hidden');
+        ageError.textContent = '';
+        isValid = true;
+    }
+    return isValid
+}
+
+function getEmployeesByDepartment(department) {
+    return employees.filter((emp) => emp.department === department)
+}
+console.log(getEmployeesByDepartment("Développement"));
+
+function augmenterSalaires(pourcentage) {
+    employees.forEach(emp => {
+        emp.salary = emp.salary * (1 + pourcentage);
+    });
+
+    displayEmployee(employees);
+}
+
+augmenterSalaires(0.25)
+
+function getDepartmentStats() {
+    // Retourne un objet avec pour chaque département :
+    // - nombre d'employés
+    // - salaire moyen
+    // - salaire maximum
+    let depTotal = {};
+    for (let i = 0; i < employees.length; i++) {
+        let depName = employees[i].department;
+
+        if (!depTotal[depName]) {
+            depTotal[depName] = { nombreEmployees: 0, AvgSalary: 0, MaxSalary: 0, total: 0 }
+        }
+        depTotal[depName].nombreEmployees += 1;
+        depTotal[depName].MaxSalary = Math.max(depTotal[depName].MaxSalary,employees[i].salary);
+        depTotal[depName].total += employees[i].salary;
+
+    }
+    for (let x in depTotal) {
+        depTotal[x].AvgSalary = depTotal[x].total / depTotal[x].nombreEmployees;
+    }
+    return depTotal
+}
+console.log(getDepartmentStats());
+
+function getEmployeesWithSkill(skill) {
+    // Retourne les employés ayant une compétence spécifique
+    return employees.filter((emp)=>emp.skills.includes(skill))
+}
+console.log(getEmployeesWithSkill("Scrum"))
